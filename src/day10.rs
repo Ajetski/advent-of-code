6683ced10e88889bc9027af85c5b307f84db9e5b
Adam Jeniski <ajeniski4@gmail.com>
2022-01-06 16:33:40 -0500

    Initial commit

---
+++ src/day10.rs
@@ -0,0 +1,16 @@
+type Stack = Vec<char>;
+
+fn part_1_solution() {
+    
+}
+
+#[cfg(test)]
+mod part1 {
+    use super::*;
+
+
+    #[test]
+    fn run_sample() {
+        part_1_solution()
+    }
+}
