2ab1f41efeaba57e738fa1c23f60deda99cc9730
Adam Jeniski <ajeniski4@gmail.com>
2022-01-07 02:46:33 -0500

    fix tests

--- src/day1.rs
+++ src/day1.rs
@@ -11,12 +11,29 @@ mod part1 {
     #[test]
     fn run_sample() {
         let input = include_str!("../inputs/1_test.txt");
-        assert_eq!(part_1_solution(input), 0);
+        assert_eq!(part_1_solution(input), 7);
     }
 
     #[test]
     fn run() {
         let input = include_str!("../inputs/1.txt");
-        assert_eq!(part_1_solution(input), 0);
+        assert_eq!(part_1_solution(input), 1139);
+    }
+}
+
+#[cfg(test)]
+mod part2 {
+    use super::*;
+
+    #[test]
+    fn run_sample() {
+        let input = include_str!("../inputs/1_test.txt");
+        assert_eq!(part_2_solution(input), 5);
+    }
+
+    #[test]
+    fn run() {
+        let input = include_str!("../inputs/1.txt");
+        assert_eq!(part_2_solution(input), 1103);
     }
 }
