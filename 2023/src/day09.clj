b9891387194c8f9bea33fd472fdb95353e026225
Adam Jeniski <adam.jeniski@flashtalking.com>
2023-12-09 09:42:07 -0500

    golf

--- 2023/src/day09.clj
+++ 2023/src/day09.clj
@@ -15,19 +15,13 @@
 
 (defn solve [reduction]
   (->> (get-puzzle-input 9)
-       (mapv parse-line)
-       (mapv #(let [pyrimid (generate-pyramid %)]
-                (loop [acc 0
-                       line-idx (dec (count pyrimid))]
-                  (if (< line-idx 0)
-                    acc
-                    (recur (reduction (get pyrimid line-idx) acc)
-                           (dec line-idx))))))
+       (map parse-line)
+       (mapv (comp reverse generate-pyramid))
+       (map (partial reduce reduction 0))
        (reduce +)))
 
 ;; part 1
-(solve (fn [line num] (+ (peek line) num)))
+(solve (fn [num line] (+ (peek line) num)))
 
 ;; part 2
-(solve (fn [line num] (- (first line) num)))
-
+(solve (fn [num line] (- (first line) num)))
