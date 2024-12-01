14ce95a390997e64ca0a2eb3ea89ae11dc2d9835
Adam <ajeniski4@gmail.com>
2024-12-01 12:10:49 -0500

    add auto-download input setup

---
+++ 2024/src/day02.clj
@@ -0,0 +1,15 @@
+(ns day02
+  (:require
+   [clojure.string :as str]))
+
+(def raw-input (str/split-lines ""))
+
+(def input (->> raw-input
+                (map identity)))
+
+;; part 1
+(->> input)
+
+;; part 2
+;; (->> input)
+
