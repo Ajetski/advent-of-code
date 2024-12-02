3e35bcad4aa5983722f60571ac084f690d0d6c15
Adam <ajeniski4@gmail.com>
2024-12-02 01:21:43 -0500

    day 2

--- 2024/src/day02.clj
+++ 2024/src/day02.clj
@@ -2,15 +2,34 @@
   (:require
    [input-manager :refer [get-input]]
    [core :as c]))
-(do
-  (def input
-    (->> (get-input 1)
-         (map identity)))
-  input)
+
+(def input (->> (get-input 2)
+                (map c/split-whitespace)
+                (map #(mapv parse-long %))))
+
+(defn get-diffs [coll]
+  (->> (map vector coll (rest coll))
+       (mapv #(apply - %))))
+
+(defn small-diffs? [diffs]
+  (or (every? #(<= 1 % 3) diffs)
+      (every? #(<= -3 % -1) diffs)))
+
+(defn remove-at-idx [v idx]
+  (concat (subvec v 0 idx) (subvec v (inc idx))))
 
 ;; part 1
-(->> input)
+(->> input
+     (mapv get-diffs)
+     (filterv small-diffs?)
+     count)
 
 ;; part 2
-;; (->> input)
+(->> input
+     (map (fn [coll]
+            (map #(remove-at-idx coll %)
+                 (range (count coll)))))
+     (map #(map get-diffs %))
+     (filter #(some small-diffs? %))
+     count)
 
